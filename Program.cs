04a550acca115e296483ac7cbddd82bfa512381e
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-17 08:46:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Topic_6___Loops_Assignment__Electric_Boogaloo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+
+            while (choice != "d")
+            {
+
+            }
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
