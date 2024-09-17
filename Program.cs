9525c724a5f9defaaf795bca605de2ef9ef8dcf4
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-17 09:27:42 -0400

    Done Prompter, now onto banking

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 
             while (choice != "d")
             {
-                Console.WriteLine("Hellow user, welcome to the menu. Please select which program you would like to run:");
+                Console.WriteLine("Hello user, welcome to the menu. Please select which program you would like to run:");
                 Console.WriteLine();
                 Console.WriteLine("a - Prompter");
                 Console.WriteLine("b - Simple Banking Machine");
@@ -36,12 +36,13 @@
                 }
                 else if (choice == "d")
                 {
-                    Console.WriteLine("You have chosen to quit, goodbye");
+                    Console.WriteLine("You have chosen to quit, goodbye.");
                 }
                 else
                 {
                     Console.WriteLine("Inlavid choice, press ENTER to continue");
                     Console.ReadLine();
+                    Console.Clear();
                 }
 
 
@@ -62,11 +63,24 @@
 
             Console.WriteLine($"The minimum value is {min}, now give me the maximum value:");
 
-            while (!Int32.TryParse(Console.ReadLine().Trim(), out max))
+            while (!Int32.TryParse(Console.ReadLine().Trim(), out max) || max < min)
             {
-                Console.WriteLine("That is not a valid input. Please input an integer to use as the minimum value.");
+                Console.WriteLine($"That is not a valid input. Please input an integer that is equal to or greater than the minimum value of {min}");
             }
 
+            Console.WriteLine($"The maximum value is {max}");
+            Console.WriteLine($"Now, enter a number between {min} and {max} (inclusive)");
+
+            while (!Int32.TryParse(Console.ReadLine().Trim(), out betweenNum) || betweenNum < min || betweenNum > max)
+            {
+                Console.WriteLine($"That is not a valid response. Please input an integer between {min} and {max}");
+            }
+
+            Console.WriteLine($"The chosen number between {min} and {max} is {betweenNum}");
+            Console.WriteLine("Thank you for using the prompter program.");
+            Console.WriteLine("Press ENTER to return to the menu");
+            Console.ReadLine();
+            Console.Clear();
         }
 
         public static void Banking()
