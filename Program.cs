580ac64023146d655ffd0a3391bd97e109bba983
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-17 09:01:52 -0400

    Stuff

--- Program.cs
+++ Program.cs
@@ -8,13 +8,61 @@
 
             while (choice != "d")
             {
+                Console.WriteLine("Hellow user, welcome to the menu. Please select which program you would like to run:");
+                Console.WriteLine();
+                Console.WriteLine("a - Prompter");
+                Console.WriteLine("b - Simple Banking Machine");
+                Console.WriteLine("c - Doubles Roller");
+                Console.WriteLine("d - Quit");
+                Console.WriteLine();
+
+                choice = Console.ReadLine().Trim().ToLower();
+
+                Console.WriteLine();
+
+                if (choice == "a")
+                {
+                    Console.WriteLine();
+                }
+                else if (choice == "b")
+                {
+                    Console.WriteLine();
+                }
+                else if (choice == "c")
+                {
+                    Console.WriteLine();
+                }
+                else if (choice == "d")
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Inlavid choice, press ENTER to continue");
+                    Console.ReadLine();
+                }
+
 
             }
-
-
-
-
-            Console.WriteLine("Hello, World!");
         }
+
+        public static void Prompter()
+        {
+
+        }
+
+        public static void Banking()
+        {
+
+        }
+
+        public static void DoublesRoller()
+        {
+
+
+        }
+
+
+
     }
 }
